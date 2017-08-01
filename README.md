dc0a9795c0138c66481ab9e6140a81963c15b498
pjchender <pjchender@gmail.com>
2017-08-01 15:07:25 +0800

    Change the gulp template for watching files

--- README.md
+++ README.md
@@ -1,9 +1,35 @@
-# This is a template of gulp to process pug, sass/scss, and js file.
 
+## Info
 
-```shell
+This is a simple browser auto-reloading app by gulp-watching and browsersync, which can watch files in your project folders and reload borwser when any file changed.
+
+## Usage
+
+Set the folder and file type you want to watch in `gulpfile.js`:
+
+```javascript
+gulp.watch('.//app/**/*.{css,js,erb,scss,vue}', ['browser-reload'])
+```
+
+You can use local server to start your app or through proxy:
+
+```javascript
+// local server
+  server: {
+    baseDir: './'
+  }
+
+// proxy: 'http://localhost:3000/'
+```
+
+Run start to watching files: 
+
+```
 npm install
 npm run start
 ```
 
-## Enjoy Coding
+## Vendor
+
+- [BrowserSync](https://browsersync.io/docs/gulp)
+- [Gulp](https://gulpjs.com/)
--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,24 @@
 const gulp = require('gulp')
-const $    = require('gulp-load-plugins')()
-const concat = require('gulp-concat')
 const browserSync = require('browser-sync').create()
-const sass = require('gulp-sass')
-const cleanCSS = require('gulp-clean-css')
-const pug = require('gulp-pug')
-// const inject = require('gulp-inject')
-
-// inject link into html
-// gulp.task('inject-index', ['pug-compile', 'sass', 'concat-js'], function () {
-//   var target = gulp.src('./dist/index.html')
-//   // It's not necessary to read the files (will speed up things), we're only after their paths:
-//   var sources = gulp.src(['./dist/*.js', './dist/*.css'], {read: false})
-
-//   return target.pipe(inject(sources, {relative: true}))
-//     .pipe(gulp.dest('./dist'))
-//     .pipe(browserSync.stream())
-// })
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['pug-compile', 'sass', 'concat-js'], function () {
+gulp.task('serve', function () {
   browserSync.init({
-    server: './dist'
+
+    // local server
+    // server: {
+    //   baseDir: './'
+    // }
+
+    // proxy
+    proxy: 'http://localhost:3000/'
   })
-
-  gulp.watch('src/views/*.pug', ['browser-reload'])
-  gulp.watch('src/sass/*.scss', ['browser-reload'])
-  gulp.watch('src/js/*.js', ['browser-reload'])
-  // gulp.watch('*.html').on('change', browserSync.reload)
+  gulp.watch('./../shopping-mall/app/**/*.{css,js,erb,scss,vue}', ['browser-reload'])
 })
 
-gulp.task('browser-reload', ['pug-compile', 'sass', 'concat-js'], function () {
-  browserSync.stream()
-})
-
-//  將不同的 js 檔案合併在同一支當中
-gulp.task('concat-js', function () {
-  return gulp.src([
-    'vendor/jquery-3.1.1.js',
-    'vendor/tether.min.js',
-    'vendor/bootstrap.min.js',
-    'vendor/lodash.min.js',
-    'src/js/*.js'
-  ])
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('./dist'))
-})
-
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function () {
-  return gulp.src('src/sass/*.scss')
-        .pipe(sass())
-        .pipe($.autoprefixer({
-          browsers: ['last 2 versions', 'ie >= 9']
-        }))
-        .pipe(gulp.dest('./dist'))
-})
-
-//  clean-css
-gulp.task('clean-css', ['sass'], function () {
-  return gulp.src(['./vendor/*.css', './dist/*.css'])
-        .pipe(cleanCSS())
-        .pipe(concat('main.css'), {newLine: ''})
-        .pipe(gulp.dest('./dist'))
-})
-
-//  將 pug 轉成 HTML
-gulp.task('pug-compile', function () {
-  return gulp.src('src/views/*.pug')
-        .pipe(pug({
-          // Your options in here.
-        }))
-        .pipe(gulp.dest('./dist'))
-        .pipe(browserSync.stream())
+gulp.task('browser-reload', function (done) {
+  browserSync.reload()
+  done()
 })
 
 gulp.task('default', ['serve'])
